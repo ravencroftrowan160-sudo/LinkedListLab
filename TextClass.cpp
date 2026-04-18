b59c05f11344115b313e13afeeef48c3c9aa48a5
ravencroftrowan160-sudo <ravencroftrowan160@gmail.com>
2026-04-18 22:19:27 +0000

    Added getHead() and removeHead() functions to align with the first task.

--- TextClass.cpp
+++ TextClass.cpp
@@ -1,5 +1,46 @@
 #include "TextClass.hpp"
 
-TextClass::TextClass(){
-    
+TextClass::TextClass():head(nullptr), tail(nullptr), current(nullptr){
+
+}
+
+TextClass::~TextClass(){
+    while(head != nullptr){
+        removeHead();
+    }
+}
+
+void TextClass::addHead(char value){
+    Node* newHead = new Node;
+    newHead->data = value;
+    newHead->next = head;
+
+    head = newHead;
+
+    if(tail == nullptr){
+        tail = head;
+    }
+}
+
+char TextClass::getHead() const{
+    if(head == nullptr){
+        throw std::out_of_range("List is empty");
+    }
+
+    return head->data;
+}
+
+void removeHead(){
+    if(head == nullptr){
+        throw std::out_of_range("List is empty");
+    }
+
+    Node * temp = head;
+    head = head->next;
+
+    if(head == nullptr){
+        tail == nullptr
+    }
+
+    delete temp;
 }
