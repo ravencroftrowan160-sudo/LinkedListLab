52eefdc4fccddacc151c70f1eaf5d14a9f69ad4f
ravencroftrowan160-sudo <ravencroftrowan160@gmail.com>
2026-04-18 20:49:41 +0000

    Added TextClass.hpp with a node struct and a constructor and destructor

---
+++ TextClass.hpp
@@ -0,0 +1,22 @@
+
+#ifndef TEXTCLASS_HPP
+#define TEXTCLASS_HPP
+
+#include <stdexception>//for exceptions
+
+struct Node{
+    char data;
+    Node* next;
+};
+
+class TextClass{
+private:
+    Node* head;
+    Node* tail;
+    Node* current;
+
+public:
+    //constructor/destructor
+    TextClass();
+    ~TextClass();
+};
